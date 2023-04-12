fc87a987431b255320dc5f4708304722e147bfbc
Vinh Hoang Cong <22026501@vnu.edu.vn>
2023-04-12 15:08:42 +0700

    updating map parser

--- src/map/MapParser.cpp
+++ src/map/MapParser.cpp
@@ -1,2 +1,40 @@
 #include "Map/MapParser.h"
 
+MapParser* MapParser::s_Instance = nullptr;
+
+bool MapParser::Load(std::string id, std::string source){
+    id = "lv1";
+    source = "src/resources/Map/Map1.tmx";
+    //currently only use 1 map and 1 lv
+    return Parse(id, source);
+}
+void MapParser::Clean(){
+
+}
+
+GameMap MapParser::GetMaps(){
+
+}
+
+bool MapParser::Parse(std::string id, std::string source){
+
+}
+Tileset MapParser::ParseTileset(TiXmlElement* xmlTileset){
+    Tileset tileset;
+    tileset.Name = xmlTileset -> Attribute("name");
+    
+    xmlTileset -> Attribute("firstgid", &tileset.FirstID);
+    xmlTileset -> Attribute("tilecount", &tileset.TileCount);
+    tileset.LastID = tileset.FirstID + tileset.TileCount - 1;
+
+    xmlTileset -> Attribute("columns", &tileset.ColCount);
+    tileset.RowCount = tileset.TileCount / tileset.ColCount;
+    xmlTileset -> Attribute("tilewidth", &tileset.TileSize);
+
+    TiXmlElement* image = xmlTileset -> FirstChildElement();
+    tileset.Source = image -> Attribute("source");
+    return tileset;    
+}
+TileLayer MapParser::ParseTileLayer (TiXmlElement* xmlLayer){
+
+}
--- src/map/MapParser.h
+++ src/map/MapParser.h
@@ -1,8 +1,27 @@
 #ifndef MAPPARSER_H
 #define MAPPARSER_H
 
+#include <map>
+#include <string>
+#include "Map/GameMap.h"
+#include "Map/TileLayer.h"
+#include "TinyXML/tinyxml.h"
+
 class MapParser{
-    
+    public:
+        bool Load(std::string id, std::string source);
+        void Clean();
+
+        inline GameMap GetMaps();
+        inline static MapParser* GetInstance() {return s_Instance = (s_Instance != nullptr) ? s_Instance : new MapParser();}
+        
+    private:
+        MapParser(){}
+        bool Parse(std::string id, std::string source);
+        Tileset ParseTileset(TiXmlElement* xmlTileset);
+        TileLayer ParseTileLayer (TiXmlElement* xmlLayer);
+        static MapParser* s_Instance;
+        std::map<std::string, GameMap*> m_MapDict; 
 };
 
 #endif
